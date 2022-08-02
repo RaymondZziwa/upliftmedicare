ebdb30615e7019b82cbfe5990ce1bb1fd759cd43
Zziwa Raymond Ian <raymondzian@gmail.com>
2022-08-02 14:20:37 +0300

    patient dashboard design is complete

--- client/Patient/src/components/Patient/Patient.js
+++ client/Patient/src/components/Patient/Patient.js
@@ -1,5 +1,14 @@
 import { AuthErrorCodes } from 'firebase/auth';
 import {Row ,Col ,Form ,Card, FormControl} from 'react-bootstrap'
+import './ds.css'
+import logo from './Imgs/logo.png'
+import youtube from './Imgs/utube.png'
+import twitter from './Imgs/twitter.png'
+import facebook from './Imgs/facebook.png'
+import email from './Imgs/email.png'
+import insta from './Imgs/instagram.png'
+import linkedIn from './Imgs/linkedin.png'
+import bg from './Imgs/bg.png'
 const PatientHomePage = () =>{
     const userName = {
         border:'3px solid #11517F',
@@ -15,7 +24,7 @@ const PatientHomePage = () =>{
         textAlign:'center',
         fontWeight:'bold',
         margin:'auto',
-        padding:'5px', 
+        padding:'5px'
     }
     const userNameParent = {
         border:'3px solid #11517F',
@@ -34,23 +43,56 @@ const PatientHomePage = () =>{
         cursor:'pointer',
     }
 
+    const logoStyle = {
+        height:'50px',
+        float:'left'
+    }
+
+    const footer = {
+       height:'30px',
+       margin:'5px',
+       cursor:'pointer'
+    }
+
+    const helpline = {
+        float:'right',
+        textAlign:'center',
+        fontWeight:'bold',
+        margin:'auto',
+        color:'#11517F',
+        fontSize:'1.2rem'
+    }
     return(
         <>
-            <Row style={{margin:'auto',marginTop:'10px',display:'flex',justifyContent:'center'}}>
-                <Col sm='12' md='12' lg='12' xl='12'>
-                    <div style={userNameParent}><p style={userName}>Raymond</p></div>
-                </Col>
-            </Row>
-            <Row style={{margin:'auto',marginTop:'10px'}}>
-                <Col sm='12' md='12' lg='12' cl='12'  style={{margin:'auto'}}>
-                    <div style={linkParent}><p style={link}>Self Examination</p></div>
-                    <div style={linkParent}><p style={link}>Call An Ambulance(beta)</p></div>
-                    <div style={linkParent}><p style={link}>Book An Appointment</p></div>
-                    <div style={linkParent}><p style={link}>Call A Doctor</p></div>
-                    <div style={linkParent}><p style={link}>Medical History</p></div>
-                    <div style={linkParent}><p style={link}>Appointment History</p></div>
-                    <div style={linkParent}><p style={link}>Edit Profile</p></div>
-                </Col>
+            <div className='bg'>
+                <Row style={{margin:'auto',marginTop:'10px',display:'flex',justifyContent:'center'}}  >
+                    <Col sm='12' md='12' lg='12' xl='12'>
+                        <img src={logo} alt='logo' style={logoStyle}/>
+                        <div style={userNameParent}><p style={userName}>Raymond</p></div>
+                    </Col>
+                </Row>
+                <Row style={{margin:'auto',marginTop:'10px'}}>
+                    <Col sm='12' md='12' lg='12' xl='12'  style={{margin:'auto',display:'flex',justifyContent:'flex-end'}}>
+                        <div style={linkParent}><p className='linkattr' style={link}>Self Examination</p></div>
+                        <div style={linkParent}><p className='linkattr' style={link}>Call An Ambulance(beta)</p></div>
+                        <div style={linkParent}><p className='linkattr' style={link}>Book An Appointment</p></div>
+                        <div style={linkParent}><p className='linkattr' style={link}>Call A Doctor</p></div>
+                        <div style={linkParent}><p className='linkattr' style={link}>Medical History</p></div>
+                        <div style={linkParent}><p className='linkattr' style={link}>Appointment History</p></div>
+                        <div style={linkParent}><p className='linkattr' style={link}>Edit Profile</p></div>
+                    </Col>
+                </Row>
+            </div>
+            <Row style={{margin:'auto',marginTop:'2px',display:'flex',justifyContent:'center'}}  >
+                    <Col sm='12' md='12' lg='12' xl='12'>
+                        <img src={insta} alt='logo' style={footer}/>
+                        <img src={twitter} alt='logo' style={footer}/>
+                        <img src={facebook} alt='logo' style={footer}/>
+                        <img src={email} alt='logo' style={footer}/>
+                        <img src={linkedIn} alt='logo' style={footer}/>              
+                        <img src={youtube} alt='logo' style={footer}/>
+                        <p style={helpline}>Customer Helpline: +256 785 987 809</p>   
+                    </Col>
             </Row>
         </>
     );
